81d1fd40b369a831c89fa7cd09974bb29ee65190
chiakisaito <a.cop.p2p2@gmail.com>
2020-03-06 12:45:04 +0900

    初コミット

---
+++ test.rb
@@ -0,0 +1,17 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+                                     :DocumentRoot => '.',
+                                     :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+                                     :Port => '3000',
+                                 })
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.mount('/task', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/task_indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'task_indicate.rb')
+server.mount('/task_goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'task_goya.rb')
+server.start
